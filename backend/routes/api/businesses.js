4652031e17245c11c25fdab20696b69dd1ad7a03
haywood-d-johnson <haywood.d.johnson@gmail.com>
2022-05-05 16:27:11 -0500

    env: push for assist. weirdness.

--- backend/routes/api/businesses.js
+++ backend/routes/api/businesses.js
@@ -29,4 +29,79 @@ router.get(
     })
 );
 
+router.post(
+    "/",
+    businessValidator,
+    asyncHandler(async (req, res, next) => {
+        const { userId, title, description, address, city, state, zip } =
+            req.body;
+        const newBusiness = {
+            userId,
+            title,
+            description,
+            address,
+            city,
+            state,
+            zip,
+            lat: 0,
+            lng: 0,
+        };
+        const business = await Business.create(newBusiness);
+
+        if (!business) {
+            const err = new Error("Sign Up failed");
+            err.status = 401;
+            err.title = "Failed";
+            err.errors = ["This business already exists. Please try again"];
+            return next(err);
+        }
+
+        return res.json({
+            business,
+        });
+    })
+);
+
+router.patch("/:id(\\d+)", businessValidator, async (req, res, next) => {
+    const id = parseInt(req.params.id, 10);
+    const business = await Business.findByPk(id);
+
+    if (business) {
+        business.description = req.body.description;
+        business.address = req.body.address;
+        business.city = req.body.city;
+        business.state = req.body.state;
+        business.zip = req.body.zip;
+
+        // the only constant
+        const check = await Business.findOne({
+            where: {
+                address: req.body.address,
+            },
+        });
+        if (check) {
+            const err = new Error("Update failed");
+            err.status = 401;
+            err.title = "Update failed";
+            err.errors = ["The adress provided already exists."];
+            return next(err);
+        } else {
+            await business.save();
+        }
+    } else {
+        res.json({ message: "There was a problem. Please try again." });
+    }
+});
+
+router.delete("/:id(\\d+)", async (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    const business = await Business.findByPk(id);
+
+    if (business) {
+        await business.destroy();
+    } else {
+        errors.push("Not found");
+    }
+});
+
 module.exports = router;
